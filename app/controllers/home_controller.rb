e417f738fb7dc53a2c40a6e0a83dd862b2640474
jerseycoder <michelle@codeforamerica.org>
2011-04-03 16:25:40 -0700

    New homepage with carousel city picture display

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    render :layout=>"homepage.html.erb"
+    render :layout=>"application.html.erb" # without this line, Rails renders to this template by default anyway
   end
 
 end
