5706e1c431c6a3ef54a13c0e345c663bfc374b7a
jerseycoder <michelle@codeforamerica.org>
2011-05-06 12:20:55 -0700

    Fixed model dependency on s3.yml

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,7 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
   if Rails.env.production?
-    has_attached_file :avatar, :storage => :s3, :s3_credentials => "#{RAILS_ROOT}/config/s3.yml", :path => "/:style/:filename"
+    has_attached_file :avatar, :storage => :s3, :path => "/:style/:filename"
   else
     has_attached_file :avatar
   end
