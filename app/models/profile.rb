f5804186a08774a0f6be4f43750b062705abdbc3
jerseycoder <michelle@codeforamerica.org>
2011-05-06 12:40:27 -0700

    Lets try editing the models again for s3 vars

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,7 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
   if Rails.env.production?
-    has_attached_file :avatar, :storage => :s3, :path => "/:style/:filename"
+    has_attached_file :avatar, :storage => :s3, :s3_credentials => { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET']}, :path => "/:style/:filename"
   else
     has_attached_file :avatar
   end
