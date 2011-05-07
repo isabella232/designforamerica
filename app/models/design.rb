9385ba10b6e8c3496e1e872d2b49fa86f657b44f
jerseycoder <michelle@codeforamerica.org>
2011-05-06 21:45:02 -0700

    Fixed bucket issues with S3 storage in models

--- app/models/design.rb
+++ app/models/design.rb
@@ -9,8 +9,8 @@ class Design < ActiveRecord::Base
   validates_presence_of :tos, :on => :create, :message => "can't be false"    
   
   if Rails.env.production?
-   has_attached_file :thumbnail, :storage => :s3, :path => "/:style/:filename"
-    has_attached_file :zip_package, :storage => :s3, :path => "/:style/:filename"
+   has_attached_file :thumbnail, :storage => :s3, :bucket => 'projekts', :s3_credentials => { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET']}
+    has_attached_file :zip_package, :storage => :s3, :bucket => 'projekts', :s3_credentials => { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET']}
   else
     has_attached_file :thumbnail
     has_attached_file :zip_package
